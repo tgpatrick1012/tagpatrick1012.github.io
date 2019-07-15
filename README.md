d88831d2a62ce4ed6aaa1de5e4c1d207eaea53f5
tgpatrick1012 <taylorpatrick1012@gmail.com>
2019-07-15 15:58:44 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# tagpatrick1012.github.io" 
---
+++ myscript.js
@@ -0,0 +1,7 @@
+console.log("Hello World!");
+
+function toggleDarkLight() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+}
